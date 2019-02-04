672c380cc8b0d8205a7eb28b13cd8180249d5ba8
Enrico Mayor <enricomayor@hotmail.com>
2019-02-04 23:48:00 +0100

    V1

---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+
+            var movie = new Movie();
+
+            movie.Name = "Interstellar";
+            movie.Id = 13;
+
+            return View(movie);
+        }
+    }
+}
